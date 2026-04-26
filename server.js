830b35553184a82c8dd56b583616e466594e1f2e
Bhimala Satyam <satyambhimalainfo@gmail.com>
2026-04-26 20:04:55 +0400

    Add files via upload

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname));
 
 const server = http.createServer(app);
 const io = new Server(server, {
